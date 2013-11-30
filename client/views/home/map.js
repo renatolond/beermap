0cdcce3ae0b872d9433f717e31a3a3e699bad5ba
Renato "Lond" Cerqueira <lond@dcc.ufrj.br>
2013-11-30 16:59:32 -0200

    Starting tutorial to show pins on maps.

---
+++ client/views/home/map.js
@@ -0,0 +1,23 @@
+Template.map.rendered = function() {
+    if (! Session.get('map'))
+        gmaps.initialize();
+
+    Deps.autorun(function() {
+        var places = [{id:1, lat:-22.9508554407987,lng:-43.18303942680359,title:"Café e Bar Águia dos Andes"}];
+        Template.map.addPlaces(places);
+    });
+}
+
+Template.map.addPlaces = function(places) {
+        _.each(places, function(place) {
+                // check if marker already exists
+                if (!gmaps.markerExists('id', place.id))
+                    gmaps.addMarker(place);
+
+        });
+        gmaps.calcBounds();
+}
+
+Template.map.destroyed = function() {
+    Session.set('map', false);
+}
