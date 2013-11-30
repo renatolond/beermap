9b928e4f2bb2ae4b5a6f8530f2c82a7b07e293cb
rengenesio <rengenesio@gmail.com>
2013-11-30 19:30:57 -0200

    InfoWindow dos marcadores com nome do bar

--- client/views/home/map.js
+++ client/views/home/map.js
@@ -3,7 +3,7 @@ Template.map.rendered = function() {
         gmaps.initialize();
 
     Deps.autorun(function() {
-        var places = [{id:1, lat:-22.9508554407987,lng:-43.18303942680359,title:"Café e Bar Águia dos Andes"}];
+        var places = [{id:1, lat:-22.9508554407987,lng:-43.18303942680359,title:"Café e Bar Águia dos Andes"}, {id:2, lat:-22.950713,lng:-43.182708, title:"Bar Odorico"}];
         Template.map.addPlaces(places);
     });
 }
