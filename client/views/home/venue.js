9f369e7d73b903fd30518db0881cc9140803d851
Renato "Lond" Cerqueira <renato.lond@gmail.com>
2013-12-01 15:08:33 -0200

    Fixing behavior on cancel and confirm buttons.

--- client/views/home/venue.js
+++ client/views/home/venue.js
@@ -9,28 +9,11 @@ Template.page.events({
 });
 
 Template.addVenue.events({
-  'click .btnAddVenueConfirm': function (event, template) {
-//     var title = template.find(".title").value;	
-//     var description = template.find(".description").value;
-//     var public = ! template.find(".private").checked;
-//     var coords = Session.get("createCoords");
-// 
-//     if (title.length && description.length) {
-//       var id = createParty({
-//         title: title,
-//         description: description,
-//         x: coords.x,
-//         y: coords.y,
-//         public: public
-//       });
-// 
-//       Session.set("selected", id);
-//       if (! public && Meteor.users.find().count() > 1)
-//         openInviteDialog();
-      Session.set("showAddVenue", false);
-//     } else {
-//       Session.set("createError",
-//                   "It needs a title and a description, or why bother?");
-//     }
+  'click .save': function (event, template) {
+    Session.set("showAddVenue", false);
+  },
+
+  'click .cancel': function () {
+    Session.set("showAddVenue", false);
   }
 });
